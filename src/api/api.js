7b6e5e5b2b1ffb33ae98fb05b9d2d7a5498af68b
Faraz Ahmed <farazfatahsoomo@gmail.com>
2026-07-20 15:47:25 +0500

    Added login feature

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://afbros-backend.onrender.com/api"
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://afbros-backend.onrender.com/api"
+    // baseURL: "http://localhost:5000/api"
 });
 
 api.interceptors.request.use((config) => {
